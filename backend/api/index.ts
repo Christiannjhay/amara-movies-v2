135c4a1a67e5835e93d6cdb69cf9cfc58a7bca6d
Christiannjhay <sugarolchristian8@gmail.com>
2024-06-17 12:33:54 +0800

    updated route

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-
+import { logErrorMiddleware, returnError } from './errorHandler';
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,23 +10,28 @@ app.get('/api/hello', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello from the backend!' });
 });
 
-app.get('/api/popular', (req: Request, res: Response, next) => {
+app.get('/api/popular', async (req: Request, res: Response, next) => {
   try {
     const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM0NDRkNDQ5ZmRkOGQ4NGUzMDMzNGZhN2U1OTFmOCIsInN1YiI6IjY1ZjY1ZmI1ZTIxMDIzMDE3ZWVlMDJkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z1fa1jT_-JUEe0N6UNX-wdvspwCNH0j4hUpE4eg6-VI'
+        Authorization: 'Bearer your_access_token_here'
       }
     };
-    fetch(url, options)
-      .then(res => res.json())
-      .then(json => console.log(json))
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+    
+    res.status(200).json(json);
   } catch (error) {
-    next(error)
+    next(error); 
   }
-})
+});
+
+app.use(logErrorMiddleware)
+app.use(returnError)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
