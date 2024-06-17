d10f65f1be0c2cdaa3b26edcba36ab793f9c9244
Christiannjhay <sugarolchristian8@gmail.com>
2024-06-17 12:30:23 +0800

    added route

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -10,6 +10,24 @@ app.get('/api/hello', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello from the backend!' });
 });
 
+app.get('/api/popular', (req: Request, res: Response, next) => {
+  try {
+    const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM0NDRkNDQ5ZmRkOGQ4NGUzMDMzNGZhN2U1OTFmOCIsInN1YiI6IjY1ZjY1ZmI1ZTIxMDIzMDE3ZWVlMDJkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z1fa1jT_-JUEe0N6UNX-wdvspwCNH0j4hUpE4eg6-VI'
+      }
+    };
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => console.log(json))
+  } catch (error) {
+    next(error)
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
