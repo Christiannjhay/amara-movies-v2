9ebc81e108148c62870622ebab2f4348bbd6e181
Christiannjhay <sugarolchristian8@gmail.com>
2024-06-17 11:30:31 +0800

    updated backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,9 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-}));
+app.use(cors());
 
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello from the backend!' });
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app;
