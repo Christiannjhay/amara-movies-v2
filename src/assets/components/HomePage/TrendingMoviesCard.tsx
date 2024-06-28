8ac16289549773300132c65f0f17638c10717280
Christiannjhay <sugarolchristian8@gmail.com>
2024-06-28 14:35:20 +0800

    updated UI

--- src/assets/components/HomePage/TrendingMoviesCard.tsx
+++ src/assets/components/HomePage/TrendingMoviesCard.tsx
@@ -228,23 +228,24 @@ export default function TrendingMoviesCard() {
               </div>
             </div>
           </div>
-          
-            <CarouselContent>
-              {movies.map((movie) => (
-                <CarouselItem key={movie.id}>
-                  <div className="w-full h-[1080px] relative bg-[#181818]">
-                    <img
-                      src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                      alt={movie.title}
-                      className="w-full h-[400px] 2xl:h-[1080px] xl:h-[700px] lg:h-[600px] md:h-[500px] sm:h-[400px]"
-                      loading="lazy"
-                    />
-                    <div className="absolute inset-0 bg-black/50"></div>
-                    <div className="absolute bottom-[65%] sm:bottom-[65%] md:bottom-[55%] lg:bottom-[60%] xl:bottom-[50%] 2xl:bottom-[39%] sm:left-4 left-4 md:left-24 text-white w-[70%]">
-                      <h1 className="text-white font-extrabold text-sm md:text-3xl lg:text-4xl">
-                        {movie.title}
-                      </h1>
-                      <div className="flex flex-row mt-2">
+
+          <CarouselContent>
+            {movies.map((movie) => (
+              <CarouselItem key={movie.id}>
+                <div className="w-full h-[1080px] relative bg-[#181818]">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                    alt={movie.title}
+                    className="w-full h-[400px] 2xl:h-[1080px] xl:h-[700px] lg:h-[600px] md:h-[500px] sm:h-[400px]"
+                    loading="lazy"
+                  />
+                  <div className="absolute inset-0 bg-black/50"></div>
+                  <div className="absolute bottom-[65%] sm:bottom-[65%] md:bottom-[55%] lg:bottom-[60%] xl:bottom-[50%] 2xl:bottom-[39%] sm:left-4 left-4 md:left-24 text-white w-[70%]">
+                    <h1 className="text-white font-extrabold text-sm md:text-3xl lg:text-4xl">
+                      {movie.title}
+                    </h1>
+                    <div className="grid grid-cols-1 md:grid-cols-12 ">
+                      <div className="flex flex-row mt-2 col-span-1">
                         <h1 className="text-slate-300 text-sm md:text-base">
                           {getYearFromDate(movie.release_date)}
                         </h1>
@@ -260,41 +261,42 @@ export default function TrendingMoviesCard() {
                         <h1 className="ml-1 text-slate-300 text-sm md:text-base">
                           {formatVoteAverage(movie.vote_average)}
                         </h1>
-                        <div className="ml-3 flex flex-row">
-                          {movie.genres.map((genre) => (
-                            <div
-                              key={genre.id}
-                              className="mr-2 text-sm md:text-base hidden md:block"
-                            >
-                              <span className="bg-gray-200 text-gray-800 rounded-full px-3 py-1 text-sm font-semibold">
-                                {genre.name}
-                              </span>
-                            </div>
-                          ))}
-                        </div>
                       </div>
-                      <h2 className="lg:text-lg xl:text-xl text-white font-light mt-2 line-clamp-3 hidden md:block">
-                        {movie.overview}
-                      </h2>
-                      <Button
-                        onClick={() => {
-                          console.log("Clicked movie ID:", movie.id);
-                          navigate(`/view-movie/${movie.id}`);
-                        }}
-                        className="bg-[#FF3131] hover:bg-red-600 text-white py-3 px-4 md:px-6 md:py-6 md:mt-4 rounded-full mt-1 flex items-center"
-                      >
-                        <PlayIcon />
-                        <h1 className="ml-2 font-normal text-xs text-white md:text-base">
-                          Watch Now
-                        </h1>
-                      </Button>
+                      <div className="flex flex-row mb-2 md:ml-28 lg:ml-24 xl:ml-24 2xl:ml-16 mt-2 col-span-11">
+                        {movie.genres.map((genre) => (
+                          <div
+                            key={genre.id}
+                            className="mr-2 text-sm md:text-base md:block"
+                          >
+                            <span className="bg-gray-200 text-gray-800 rounded-full px-3 py-1 text-xs lg:text-sm md:text-xs font-semibold">
+                              {genre.name}
+                            </span>
+                          </div>
+                        ))}
+                      </div>
                     </div>
+                   
+                    <h2 className="lg:text-lg xl:text-xl text-white font-light mt-2 line-clamp-3 hidden md:block">
+                      {movie.overview}
+                    </h2>
+                    <Button
+                      onClick={() => {
+                        console.log("Clicked movie ID:", movie.id);
+                        navigate(`/view-movie/${movie.id}`);
+                      }}
+                      className="bg-[#FF3131] hover:bg-red-600 text-white py-3 px-4 md:px-6 md:py-6 md:mt-4 rounded-full mt-1 flex items-center"
+                    >
+                      <PlayIcon />
+                      <h1 className="ml-2 font-normal text-xs text-white md:text-base">
+                        Watch Now
+                      </h1>
+                    </Button>
                   </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
-      
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
     </div>
   );
