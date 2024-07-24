10b62530abb7e190bedabd2e44f0d9533078e903
Christiannjhay <sugarolchristian8@gmail.com>
2024-07-24 21:51:24 +0800

    updated tooltip

--- src/assets/components/ViewMovie/ViewMovieRecommendedCard.tsx
+++ src/assets/components/ViewMovie/ViewMovieRecommendedCard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import MovieCard from "../HomePage/MovieCard";
 import MovieTooltip from "../MovieTooltip";
 
