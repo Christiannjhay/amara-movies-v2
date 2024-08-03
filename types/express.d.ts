65d75b5c1eca803464acb76da358e07d74454d29
Christiannjhay <sugarolchristian8@gmail.com>
2024-08-03 15:48:22 +0800

    added auth

---
+++ types/express.d.ts
@@ -0,0 +1,11 @@
+
+import { Express } from 'express-serve-static-core';
+import { JwtPayload } from 'jsonwebtoken';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload;
+    }
+  }
+}
